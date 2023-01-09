c83fa69ac3d1f636503c97d44049a5c263ea75b9
bomin2github <122309903+bomin2github@users.noreply.github.com>
2023-01-10 06:28:58 +0900

    Delete check_update_ms.sql

--- check_update_ms.sql
+++
@@ -1,6 +0,0 @@
-update tbnodesreal set tasknode=1
-  where nodeid in (
-    select nodeid from viewnodes
-      where nodetype in (1,4) and existtype <> 3
-      and cast(VirobotReserved1 as nvarchar(10)) < convert(nvarchar(10),dateadd(day,-7,lastconntime),120)
-  )
